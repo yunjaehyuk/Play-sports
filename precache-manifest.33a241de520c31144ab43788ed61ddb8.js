cd0979bd4f7b2425502ac6a741e595eb003dd389
yunjaehyuk <db2011307@gmail.com>
2022-08-26 20:26:57 +0900

    Updates

---
+++ precache-manifest.33a241de520c31144ab43788ed61ddb8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2acb8a58a34a7a7b771d",
+    "url": "%7Byunjaehyuk%7D.github.io/%7Bproject2%7D/static/css/main.967cd25d.chunk.css"
+  },
+  {
+    "revision": "2acb8a58a34a7a7b771d",
+    "url": "%7Byunjaehyuk%7D.github.io/%7Bproject2%7D/static/js/main.2acb8a58.chunk.js"
+  },
+  {
+    "revision": "642af11f2d901ec30689",
+    "url": "%7Byunjaehyuk%7D.github.io/%7Bproject2%7D/static/css/1.c500c683.chunk.css"
+  },
+  {
+    "revision": "642af11f2d901ec30689",
+    "url": "%7Byunjaehyuk%7D.github.io/%7Bproject2%7D/static/js/1.642af11f.chunk.js"
+  },
+  {
+    "revision": "92114eb2e21c76609f12",
+    "url": "%7Byunjaehyuk%7D.github.io/%7Bproject2%7D/static/js/runtime~main.92114eb2.js"
+  },
+  {
+    "revision": "58d74e0d25c529dd22ff28372e8a9384",
+    "url": "%7Byunjaehyuk%7D.github.io/%7Bproject2%7D/static/media/messi.58d74e0d.jpg"
+  },
+  {
+    "revision": "2287120bd3a9f842fdbe79c0844e39fa",
+    "url": "%7Byunjaehyuk%7D.github.io/%7Bproject2%7D/index.html"
+  }
+];
